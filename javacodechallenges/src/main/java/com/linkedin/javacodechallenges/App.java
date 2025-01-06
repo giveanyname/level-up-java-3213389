8f5b44af3b62af2d446f5f15bdcc829a5b00cbf5
Sripooja Manimuthu <103139888+giveanyname@users.noreply.github.com>
2025-01-06 09:49:59 +0000

    4ex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,8 +5,15 @@ import java.util.Scanner;
 public class App {
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        double minimumGallonAmount = 18.84;
+        double numberOfGallonInCCF = 748;
+        if (gallonsUsage <= numberOfGallonInCCF) {
+            return minimumGallonAmount;
+        } else {
+            double extraGallonUsage = gallonsUsage - numberOfGallonInCCF;
+            double extraGallonUsageAmount = Math.ceil(extraGallonUsage / numberOfGallonInCCF) * 3.9;
+            return (minimumGallonAmount + extraGallonUsageAmount);
+        }
     }
 
     public static void main(String[] args) {
