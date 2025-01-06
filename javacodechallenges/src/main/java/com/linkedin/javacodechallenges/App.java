28d7b3b2284a7743be1db3d7addf015715a67ccf
Sripooja Manimuthu <103139888+giveanyname@users.noreply.github.com>
2025-01-06 11:41:55 +0000

    map ex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,14 +1,34 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class App {
     public static List<String> findStudentsWithIncompleteVolunteerEvents(
             List<String> students,
             Map<String, List<String>> attendeesMapping) {
         // TODO: implement function
-        return List.of();
+        Map<String, Integer> studentAttendees = new HashMap<>();
+        for(String student : students) {
+            studentAttendees.put(student, 0);
+        }
+        // the above enhanced for loop along with map defclaratgion can be written as 
+        //Map<String, Integer> studentAttendees = students.stream().collect(Collectors.toMap(s->s, n->0));
+        for(Entry<String, List<String>> valueMap : attendeesMapping.entrySet()) {
+            for(String student : valueMap.getValue()) {
+             if((studentAttendees).containsKey(student)) {
+                int count =  studentAttendees.get(student);
+                count++;
+                studentAttendees.put(student,count);
+            }
+            }
+        }
+        
+        return studentAttendees.entrySet().stream().filter(s->s.getValue() < 2).map(s->s.getKey()).toList();
     }
 
     public static void main(String[] args) {
