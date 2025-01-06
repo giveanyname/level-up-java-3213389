d9fed0be4fbd3837c2fff3fd2330ed1237e6f47e
Sripooja Manimuthu <103139888+giveanyname@users.noreply.github.com>
2025-01-06 10:52:12 +0000

    date ex

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,18 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    // Create function to calculate the date that's 
-    // 100 days from now
+import java.time.LocalDate;
+import java.time.ZoneId;
 
-    public static void main( String[] args )
-    {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+public class App {
+    // Create function to calculate the date that's
+    // 100 days from now
+    public static String getDay(LocalDate tDate) {
+        return tDate.plusDays(100).toString();
+    }
+
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("100 days from now is... " + getDay(today)
+        /* add function call */);
     }
 }
